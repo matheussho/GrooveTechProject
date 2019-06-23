a5ce672e6c30c94fe6c112308c41276d778c0b27
matheussho <matheussholiveira@gmail.com>
2019-06-23 20:14:42 -0300

    Corrigindo RegisterEmailTest

--- desafio-aut-web/src/test/java/br/com/web/groove/tech/tests/RegisterEmailTest.java
+++ desafio-aut-web/src/test/java/br/com/web/groove/tech/tests/RegisterEmailTest.java
@@ -19,7 +19,7 @@ public class RegisterEmailTest extends BaseTest {
 	RegisterSteps register = new RegisterSteps();
 
 	@Test
-	@Title("[SMOKETEST - 001] - Registrar conta e-mail com sucesso")
+	@Title("[SMOKETEST - 001] - Preencher formulario com sucesso")
 	public void testRealizarCadastroComSucesso() {
 		authetication.authenticateEmail(TestData.email);
 		register.registerUser(TestData.firstName, TestData.lastName, TestData.password, TestData.day, TestData.month,
@@ -31,7 +31,33 @@ public class RegisterEmailTest extends BaseTest {
 	}
 
 	@Test
-	@Title("[SMOKETEST - 002] - Verificar Obrigatoriedade Password")
+	@Title("[SMOKETEST - 002] - Verificar Obrigatoriedade do campo 'FirstName'")
+	public void testVerificarObrigatoriedadeFirstName() {
+		String firstNameIsEmpty = "";
+		authetication.authenticateEmail(Generator.gerarStringAleatoria() + "@gmail.com");
+		register.registerUser(firstNameIsEmpty, TestData.lastName, TestData.password, TestData.day, TestData.month,
+				TestData.year, TestData.company, TestData.address1, TestData.address2, TestData.city, TestData.state,
+				TestData.postalCode, TestData.informationAdditional, TestData.homePhone, TestData.mobilePhone,
+				TestData.alias);
+		// Verifica se o alert apareceu na tela.
+		ValidationResult.assertMessageInElement(register.divMsgError, "firstname is required.");
+	}
+
+	@Test
+	@Title("[SMOKETEST - 003] - Verificar Obrigatoriedade do campo 'LastName'")
+	public void testVerificarObrigatoriedadeLastName() {
+		String lastNameIsEmpty = "";
+		authetication.authenticateEmail(Generator.gerarStringAleatoria() + "@gmail.com");
+		register.registerUser(TestData.firstName, lastNameIsEmpty, TestData.password, TestData.day, TestData.month,
+				TestData.year, TestData.company, TestData.address1, TestData.address2, TestData.city, TestData.state,
+				TestData.postalCode, TestData.informationAdditional, TestData.homePhone, TestData.mobilePhone,
+				TestData.alias);
+		// Verifica se o alert apareceu na tela.
+		ValidationResult.assertMessageInElement(register.divMsgError, "lastname is required.");
+	}
+
+	@Test
+	@Title("[SMOKETEST - 004] - Verificar Obrigatoriedade do campo 'Password'")
 	public void testVerificarObrigatoriedadePassword() {
 		String passwordIsEmpty = "";
 		authetication.authenticateEmail(Generator.gerarStringAleatoria() + "@gmail.com");
@@ -41,6 +67,46 @@ public class RegisterEmailTest extends BaseTest {
 				TestData.alias);
 		// Verifica se o alert apareceu na tela.
 		ValidationResult.assertMessageInElement(register.divMsgError, "passwd is required.");
+	}
+
+	@Test
+	@Title("[SMOKETEST - 005] - Verificar Obrigatoriedade do campo 'Address'")
+	public void testVerificarObrigatoriedadeAddress() {
+		String addressIsEmpty = "";
+		authetication.authenticateEmail(Generator.gerarStringAleatoria() + "@gmail.com");
+		register.registerUser(TestData.firstName, TestData.lastName, TestData.password, TestData.day, TestData.month,
+				TestData.year, TestData.company, addressIsEmpty, TestData.address2, TestData.city, TestData.state,
+				TestData.postalCode, TestData.informationAdditional, TestData.homePhone, TestData.mobilePhone,
+				TestData.alias);
+		// Verifica se o alert apareceu na tela.
+		ValidationResult.assertMessageInElement(register.divMsgError, "address1 is required.");
+	}
+
+	@Test
+	@Title("[SMOKETEST - 006] - Verificar Obrigatoriedade do campo 'City'")
+	public void testVerificarObrigatoriedadeCity() {
+		String cityIsEmpty = "";
+		authetication.authenticateEmail(Generator.gerarStringAleatoria() + "@gmail.com");
+		register.registerUser(TestData.firstName, TestData.lastName, TestData.password, TestData.day, TestData.month,
+				TestData.year, TestData.company, TestData.address1, TestData.address2, cityIsEmpty, TestData.state,
+				TestData.postalCode, TestData.informationAdditional, TestData.homePhone, TestData.mobilePhone,
+				TestData.alias);
+		// Verifica se o alert apareceu na tela.
+		ValidationResult.assertMessageInElement(register.divMsgError, "city is required.");
+	}
+
+	@Test
+	@Title("[SMOKETEST - 007] - Verificar Obrigatoriedade do campo 'PostalCode'")
+	public void testVerificarObrigatoriedadePostalCode() {
+		String postalCodeIsEmpty = "";
+		authetication.authenticateEmail(Generator.gerarStringAleatoria() + "@gmail.com");
+		register.registerUser(TestData.firstName, TestData.lastName, TestData.password, TestData.day, TestData.month,
+				TestData.year, TestData.company, TestData.address1, TestData.address2, TestData.city, TestData.state,
+				postalCodeIsEmpty, TestData.informationAdditional, TestData.homePhone, TestData.mobilePhone,
+				TestData.alias);
+		// Verifica se o alert apareceu na tela.
+		ValidationResult.assertMessageInElement(register.divMsgError,
+				"The Zip/Postal code you've entered is invalid. It must follow this format: 00000");
 	}
 
 }
